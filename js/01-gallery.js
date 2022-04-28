4eda1cd1a5efaec7f5356e066902640e2f1ca31f
Volodymyr Shkuratovskyi <91778791+VSHKURAT@users.noreply.github.com>
2022-04-28 12:45:36 +0300

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,83 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+// const galleryArrayMarkup = []
+
+// galleryItems.forEach( (element)=>{
+// const galleryDiv = document.createElement("div")
+// galleryDiv.classList.add("gallery__item")
+// galleryDiv.innerHTML = `
+// <a class="gallery__link" href="${element.original}">
+// <img
+//   class="gallery__image"
+//   src="${element.preview}"
+//   data-source="${element.original}"
+//   alt="${element.description}"
+// />
+// </a>
+// `
+// galleryArrayMarkup.push(galleryDiv)
+// } )
+
+const gallery = document.querySelector(".gallery")
+const galleryMarup =  createMarkup (galleryItems)
+gallery.innerHTML = galleryMarup
+// gallery.append(...galleryArrayMarkup)
+function createMarkup (galleryItems){
+  return galleryItems.map(({preview,original,description}) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+  }).join("")
+}
+
+
+gallery.addEventListener('click', openModal)
+
+function openModal(e){
+  e.preventDefault()
+  const image = e.target.classList.contains('gallery__image')
+
+  if(!image){
+    return
+  } else{
+
+  // window.addEventListener('keydown', event => {
+  //     if( event.key ==="Escape")
+  //     instance.close()
+  //   }) 
+
+
+  
+const instance = basicLightbox.create(
+`
+<div class="modal">
+<img src="${e.target.dataset.source}" alt = "${e.target.alt}"width="800" height="600">
+        
+    </div>`
+)
+
+instance.show()
+
+gallery.addEventListener('keydown', event => {
+      if( event.key === "Escape"){
+      instance.close()
+      console.log(`pra`)}
+      return  
+    }) 
+}
+
+ }
+
+ 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// // markup
+// const galleryArrayMarkup = []
+// galleryItems.forEach( (element)=>{
+// const galleryRef = document.createElement("a")
+// galleryRef.classList.add("gallery__item")
+// galleryRef.href = element.original
+// galleryRef.innerHTML = `
+// <img
+//   class="gallery__image"
+//   src="${element.preview}"
+//   alt="${element.description}"
+//   href = "${ element.original }"
+// />
+// `
+
+
+// galleryArrayMarkup.push(galleryRef)
+// } )
+// // add markup
+// const gallery = document.querySelector('.gallery')
+// gallery.append(...galleryArrayMarkup)
+// //
+
+const gallery = document.querySelector('.gallery')
+const galleryMarkup = createGalleryMarkup(galleryItems)
+gallery.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function  createGalleryMarkup(galleryItems){
+return galleryItems.map(({preview,original,description}) => {
+  return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `
+}).join('')
+}
+
+const lightbox = new SimpleLightbox('.gallery a', 
+{captionsData: "alt",
+captionPosition	:'bottom',
+captionDelay :	250});
